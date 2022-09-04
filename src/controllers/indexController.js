a835c0e4e307a2a22308535019bc269b0c8462f4
schavanne <sir_sirchu@hotmail.com>
2022-09-04 12:25:41 -0300

    se creo products.js

---
+++ src/controllers/indexController.js
@@ -0,0 +1,17 @@
+const products = require('../data/products');
+
+
+module.exports = {
+    index : (req,res) => {
+        return res.render('index',{
+            products
+        })
+    },
+    detail : (req,res) => {
+        const {id} = req.params;
+        const product = products.find(product => product.id === +id);
+        return res.render('detalleMenu', {
+            product
+        })
+    }
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+const {index,detail} = require('../controllers/indexController');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router
+  .get('/', index)
+  .get('/detail/:id', detail)
 
 module.exports = router;
